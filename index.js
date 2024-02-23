0eaa54c6e40545064aec472b22d26dca150fe328
Stephaniee <62786809+StephanieAgatha@users.noreply.github.com>
2024-02-24 01:06:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const { multiply, add, subtract, divide } = require('mtksora');
+
+console.log(multiply(2, 3)); // Output: 6
+console.log(add(5, 7));      // Output: 12
+console.log(subtract(10, 4));// Output: 6
+console.log(divide(8, 2));   // Output: 4
